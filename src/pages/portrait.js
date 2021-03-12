020df66570ec60424b9c39c7ea1f7a204313a982
nison79 <georgenison@gmail.com>
2021-03-12 19:41:46 +0200

    implement drawings

--- src/pages/portrait.js
+++ src/pages/portrait.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 import ImageGridPortrait from '../components/ImageGridPortrait'
+import './portrait.module.scss'
 
 const portrait = () => {
   return (
     <Layout>
+    <div className="back">
+      <StaticImage  
+        src='../images/back.png' 
+        placeholder="blurred"
+        layout="constrained"
+        width={1500}
+        
+        /> 
+    </div>
       <h1>Portrait</h1>
       <ImageGridPortrait />
     </Layout>
